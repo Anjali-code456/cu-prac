86ce7543d001e1281ff1a75827d8e9375e24c4ff
Anjali-code456 <anjali3995647@gmail.com>
2025-08-29 13:46:41 +0530

    Add files via upload

---
+++ cu.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    document.getElementById('pop').style.display='block';
+}
+document.getElementById('pop_close').onclick=function(){
+    document.getElementById('pop').style.display='none';
+}
